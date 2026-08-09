56ec29589b70eec81692b8f5ae169da5b82863a5
Artem Chupylko <achupylko@gmail.com>
2026-08-09 18:32:07 +0000

    feat: add generic localStorage API

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,28 @@
+export const loadFromLocalStorage = <T>(key: string): T | null => {
+  try {
+    const value = localStorage.getItem(key);
+
+    if (value === null) {
+      return null;
+    }
+
+    return JSON.parse(value) as T;
+  } catch (error) {
+    console.error(`Failed to load "${key}" from localStorage:`, error);
+    return null;
+  }
+};
+
+export const saveToLocalStorage = <T>(key: string, value: T): boolean => {
+  try {
+    const serializedValue = JSON.stringify(value);
+
+    localStorage.setItem(key, serializedValue);
+
+    return true;
+  } catch (error) {
+    console.error(`Failed to save "${key}" to localStorage:`, error);
+
+    return false;
+  }
+};
